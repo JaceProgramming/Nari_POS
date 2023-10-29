865220ec7d0e2ec7496bc4e511c762763bfece43
재원 Jace <jaewonjace@jaewons-MacBook-Pro.local>
2023-10-29 23:11:20 +0800

    Update with banana

--- src/App.js
+++ src/App.js
@@ -37,41 +37,42 @@ const App = () => {
   let TH6 = 3600;
 
   const menuList = [
-    { id: 1, name: "우유생크림", price: F2, color: "BONE1" },
-    { id: 2, name: "딸기우유", price: F4, color: "BONE1" },
-    { id: 3, name: "슈크림", price: F5, color: "BONE1" },
-    { id: 4, name: "초코우유", price: F6, color: "BONE1" },
-    { id: 5, name: "인절미", price: F6, color: "BONE1" },
-    { id: 6, name: "크림브륄뤠", price: F6, color: "BONE1" },
-    { id: 7, name: "황치즈", price: F6, color: "BONE1" },
-    { id: 8, name: "애플시나몬", price: F6, color: "BONE1" },
+    { id: 1, name: "우유생크림", price: F2, color: "color1" },
+    { id: 2, name: "딸기우유", price: F4, color: "color2" },
+    { id: 3, name: "슈크림", price: F5, color: "color3" },
+    { id: 4, name: "초코우유", price: F6, color: "color4" },
+    { id: 5, name: "인절미", price: F6, color: "color4" },
+    { id: 6, name: "크림브륄뤠", price: F6, color: "color4" },
+    { id: 7, name: "황치즈", price: F6, color: "color4" },
+    { id: 8, name: "애플시나몬", price: F6, color: "color4" },
+    { id: 9, name: "바나나우유", price: F6, color: "color4" },
     //
-    { id: 9, name: "블루베리크림치즈", price: F8, color: "BONE2" },
-    { id: 10, name: "돼지바", price: F8, color: "BONE2" },
-    { id: 11, name: "밤라떼", price: F8, color: "BONE2" },
-    { id: 12, name: "레몬생크림", price: F8, color: "BONE2" },
-    { id: 13, name: "생크림카스테라", price: F8, color: "BONE2" },
+    { id: 10, name: "블루베리크림치즈", price: F8, color: "color5" },
+    { id: 11, name: "돼지바", price: F8, color: "color5" },
+    { id: 12, name: "밤라떼", price: F8, color: "color5" },
+    { id: 13, name: "레몬생크림", price: F8, color: "color5" },
+    { id: 14, name: "생크림카스테라", price: F8, color: "color5" },
     //
-    { id: 14, name: "황치즈", price: TH6, color: "BABYPINK" },
-    { id: 15, name: "애플잼 크럼블", price: TH8, color: "BABYPINK" },
-    { id: 16, name: "피넛 딸기잼 크럼블", price: TH8, color: "BABYPINK" },
-    { id: 17, name: "솔티 초코칩", price: TH8, color: "BABYPINK" },
-    { id: 18, name: "약과 쿠키", price: F5, color: "BABYPINK" },
-    { id: 19, name: "아이스크림 S", price: F0, color: "BUBBLEGUM" },
-    { id: 20, name: "아이스크림 M", price: S0, color: "BUBBLEGUM" },
-    { id: 21, name: "아이스크림 L", price: TEN0, color: "BUBBLEGUM" },
+    { id: 15, name: "황치즈", price: TH6, color: "color6" },
+    { id: 16, name: "애플잼 크럼블", price: TH8, color: "color6" },
+    { id: 17, name: "피넛 딸기잼 크럼블", price: TH8, color: "color6" },
+    { id: 18, name: "솔티 초코칩", price: TH8, color: "color6" },
+    { id: 19, name: "약과 쿠키", price: F5, color: "color6" },
+    { id: 20, name: "아이스크림 S", price: F0, color: "color3" },
+    { id: 21, name: "아이스크림 M", price: S0, color: "color2" },
+    { id: 22, name: "아이스크림 L", price: TEN0, color: "color1" },
   ];
 
   const menuGroups = [
-    menuList.slice(0, 8),
-    menuList.slice(8, 13),
-    menuList.slice(13, 21),
+    menuList.slice(0, 9),
+    menuList.slice(9, 14),
+    menuList.slice(14, 22),
   ];
 
   return (
     <div className="app">
       <div className="calculator">
-        <p className="CalCulName">의정부 나리꼬모 파이팅 &gt;,&lt; </p>
+        <p className="CalCulName">의정부 나리꼬모 포스기 </p>
         <div className="selection-summary">
           <p>
             Selected Menus: {selectedMenus.map((menu) => menu.name).join(", ")}
